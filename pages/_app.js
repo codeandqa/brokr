c2a1b22b973a96979bbc3854c6f5fb7f808b544f
Aditya Kumar <akumar-pcres@mac.lan>
2025-07-31 02:34:52 -0400

    added header page and logout page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 // pages/_app.js
 import '../styles/globals.css'
+import Header from '../components/Header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className="pt-6">
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
